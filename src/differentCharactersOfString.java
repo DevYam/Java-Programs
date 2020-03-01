ade646337a3a5434036098185a78856201779eae
Divyam Kumar <divyam.kumar@sap.com>
2020-03-01 12:37:55 +0530

    Written something in Different characters using java

--- src/differentCharactersOfString.java
+++ src/differentCharactersOfString.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class differentCharactersOfString {
+    public static void main(String[] args) {
+        System.out.println("Enter a string to extract its characters");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char arr[] = str.toCharArray();
+        for (int i=0; i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
 }
+
+// Smaple input "This is a  shitty program"
+// Sample output "T h i s   i s   a     s h i t t y   p r o g r a m "
+// ;) Don't laugh :)
