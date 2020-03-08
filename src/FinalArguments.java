60953e8ed01307560a587226f90e20f1a37522c4
Divyam Kumar <divyam.kumar@sap.com>
2020-03-08 11:01:04 +0530

    Changes in final argument

--- src/FinalArguments.java
+++ src/FinalArguments.java
@@ -1,7 +1,7 @@
 public class FinalArguments {
     // This is a class where the method below has a final type of argument
     public static void sum(final int a, int b){
-//     a=50;     // Writing this gives a compile time error -> final
+//     a=50;     // Writing this gives a compile time error -> final parameter can not be changed
         System.out.println("Sum is "+(a+b));
     }
 
