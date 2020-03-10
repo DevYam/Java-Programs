5f1b9ec176f6eaf5e7a566d9d33ff55ef2d32e25
Divyam Kumar <divyam.kumar@sap.com>
2020-03-10 22:31:48 +0530

    Implemented Arrays.equals method

--- src/ArraysMethods.java
+++ src/ArraysMethods.java
@@ -7,6 +7,13 @@ public class ArraysMethods {
         int arr[] = {10,20,30,40,50};
         int searchResult = Arrays.binarySearch(arr,30);
         System.out.println("Search result is  = "+searchResult);  // Result is 2, it returns the array index which is 2
+
+        // Now checking the implementation of equals method
+        int arr2[] = {10,20,30,40,50};
+        if (Arrays.equals(arr,arr2)){
+            System.out.println("Both arrays are equal");
+        }else
+            System.out.println("They are not equal");
     }
 
 }
