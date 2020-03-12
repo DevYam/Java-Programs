124e411d76d50f06b38bb892c73166746bb34739
Divyam Kumar <divyam.kumar@sap.com>
2020-03-12 09:29:43 +0530

    asked questions modifications

--- src/AskedQuestion.java
+++ src/AskedQuestion.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class AskedQuestion {
+    public static void main(String[] args) {
+        String str = "aakdshfkjhsd123hg3gg4214hj214141g1jkkjgdjhgs@>,dfds!223(dsjfhf4849dfhks";
+       String str1 = str.replaceAll("[^A-Za-z0-9]","");
+        System.out.println(str1);
+        char arr[] = str1.toCharArray();
+        Arrays.sort(arr);
+        String str2 = new String(arr);
+        System.out.println(str2);
+    }
 }
