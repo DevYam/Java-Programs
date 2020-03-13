40b4a3d7bc9b0583616d5eec4c54f6c0c6fd332a
Divyam Kumar <divyam.kumar@sap.com>
2020-03-13 09:59:47 +0530

    Added method to character array

---
+++ src/ToCharArrayMethodOfStingClass.java
@@ -0,0 +1,8 @@
+public class ToCharArrayMethodOfStingClass {
+    public static void main(String[] args) {
+        System.out.println("Thomas Alva Edition".length());
+        char name [] = "Thomas Alva Edition".toCharArray();
+        System.out.println(name.length);
+        System.out.println(name);
+    }
+}
