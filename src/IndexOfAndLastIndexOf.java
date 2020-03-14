1c23e9cc526d3938931d9e8a3e7ab37d78c05624
Divyam Kumar <divyam.kumar@sap.com>
2020-03-14 21:15:00 +0530

    Changes to the index of and last index of java program

--- src/IndexOfAndLastIndexOf.java
+++ src/IndexOfAndLastIndexOf.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class IndexOfAndLastIndexOf {
+    public static void main(String[] args) {
+        String s = "Now is the Time For All Indians";
+        s = s+"to come to the aid og India";
+
+        System.out.println(s.indexOf("x")); //output -> -1
+        System.out.println(s.indexOf("a")); //output -> 28
+        System.out.println(s.lastIndexOf("a"));  //output -> 57
+        System.out.println(s.indexOf("Ind"));   // 24
+        System.out.println(s.lastIndexOf("Ind")); // 53
+        System.out.println(s.indexOf("Ind",30)); // 52
+
+
+    }
 }
