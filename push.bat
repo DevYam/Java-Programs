292e633e91909dc75b9ae33908148f4dac117715
Divyam Kumar <divyam.kumar@sap.com>
2020-03-24 15:05:27 +0530

    test

---
+++ push.bat
@@ -0,0 +1,8 @@
+
+@echo off
+git status
+git add .
+echo Input Commit message : 
+set /p msg=""
+git commit -m %msg%
+git push origin master
--- src/HashCodeMethod.java
+++ src/HashCodeMethod.java
@@ -10,5 +10,8 @@ public class HashCodeMethod {
         System.out.println(a1.hashCode());      // hashcode for a1 and a3 is same as they are pointing to same memory location  460141958
         System.out.println(a2.hashCode());
         System.out.println(a3.hashCode());      // 460141958
+
+        System.out.println(a1.equals(a3));      // True     Because hashcode value of a1 and a3 are same
+        System.out.println(a1.equals(a2));      // False
     }
 }
