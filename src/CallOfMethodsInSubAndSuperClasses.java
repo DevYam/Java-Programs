2e5aee59cac3d7e1dcea0dcc33add3f8efd952db
Divyam Kumar <divyam.kumar@sap.com>
2020-03-24 13:24:00 +0530

    Showing the flow of method call in extended classes

---
+++ src/CallOfMethodsInSubAndSuperClasses.java
@@ -0,0 +1,33 @@
+class class1{
+    void call(){
+        System.out.println("This is class 1");
+    }
+}
+
+class class2 extends class1{
+
+
+}
+
+class class3 extends class2{
+
+}
+
+class class4 extends class3{
+//    void call(){
+//        System.out.println("This is class 4");
+//    }
+
+}
+
+
+
+
+
+public class CallOfMethodsInSubAndSuperClasses {
+    public static void main(String[] args) {
+        class4 obj = new class4();
+        obj.call(); // This calls method in class 1
+
+    }
+}
