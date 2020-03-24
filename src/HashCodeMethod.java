7b0ecdd4876fe0996cd11322866bfb5ab9dba205
Divyam Kumar <divyam.kumar@sap.com>
2020-03-24 15:09:52 +0530

    Some more changes

--- src/HashCodeMethod.java
+++ src/HashCodeMethod.java
@@ -12,6 +12,6 @@ public class HashCodeMethod {
         System.out.println(a3.hashCode());      // 460141958
 
         System.out.println(a1.equals(a3));      // True     Because hashcode value of a1 and a3 are same
-        System.out.println(a1.equals(a2));      // False    Because not same
+        System.out.println(a1.equals(a2));      // False    Because Hashcode not same
     }
 }
