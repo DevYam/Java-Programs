efb9c32f9de16aa26308b0bf2999fc3010196e14
Divyam <divyamkumarsingh@gmail.com>
2020-04-05 02:35:38 +0530

    Added comments

--- src/MethodHidingEx1.java
+++ src/MethodHidingEx1.java
@@ -15,7 +15,7 @@ public class MethodHidingEx1 {
     public static void main(String[] args) {
         Parent001 parent001 = new Parent001();
         parent001.show(10,20);// Calls method sum from parent
-        parent001 = new child001();
+        parent001 = new child001(); // parent variable pointing child object
         parent001.show(30,40); // Calls method sum from parent
     }
 
