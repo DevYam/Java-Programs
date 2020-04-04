7682e2858696f4c237a0d999555e5ea0fff02806
Divyam <divyamkumarsingh@gmail.com>
2020-04-05 02:24:19 +0530

    Method hiding

---
+++ src/MethodHidingEx1.java
@@ -0,0 +1,22 @@
+class Parent001{
+    public static void show(int a, int b){
+        System.out.println("Sum from parent is "+(a+b));
+    }
+}
+
+class child001 extends Parent001{
+    public static void show(int a, int b){
+        System.out.println("Sum from child method "+(a+b));
+    }
+}
+
+
+public class MethodHidingEx1 {
+    public static void main(String[] args) {
+        Parent001 parent001 = new Parent001();
+        parent001.show(10,20);// Calls method sum from parent
+        parent001 = new child001();
+        parent001.show(30,40); // Calls method sum from parent
+    }
+
+}
