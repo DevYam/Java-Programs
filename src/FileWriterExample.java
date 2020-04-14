d654686fbad5c3950484fa101d87e00a6c1d9ba2
Divyam <divyamkumarsingh@gmail.com>
2020-04-15 02:17:22 +0530

    Changes to file writer added append

--- src/FileWriterExample.java
+++ src/FileWriterExample.java
@@ -3,8 +3,8 @@ import java.io.IOException;
 
 public class FileWriterExample {
     public static void main(String[] args) throws IOException {
-        FileWriter fw = new FileWriter("C:\\Users\\I516379\\Desktop\\new.txt");
-        String content = "This is written using java";
+        FileWriter fw = new FileWriter("C:\\Users\\I516379\\Desktop\\new.txt",true);
+        String content = "This is written using java  ";
             fw.write(content);
         fw.close();
     }
