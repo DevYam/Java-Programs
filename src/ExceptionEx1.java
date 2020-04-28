cedae8ebd8e20eeff656bb6dfae2bc6eecd7f5ca
Divyam <divyamkumarsingh@gmail.com>
2020-04-28 14:56:16 +0530

    Changes to comments in exception

--- src/ExceptionEx1.java
+++ src/ExceptionEx1.java
@@ -4,7 +4,7 @@ public class ExceptionEx1 {
         System.out.println("Hello Exceptions"); // This will be printed
         System.out.println(10/0);
         System.out.println("I seem to have exceptions"); //Since exceptions are caught prior to this this will not be printed
-        // These are checked exceptions where we will have to tell jre to provide default handler or
+        // These are Un - checked exceptions i.e, not checked by jre where we will have to tell jre to provide default handler or
         // we will have to handle the by ourselves
 
     }
--- src/ExceptionEx2.java
+++ src/ExceptionEx2.java
@@ -1,5 +1,6 @@
 public class ExceptionEx2 {
     // This will be a null pointer exception
+    // This is an unchecked exception i.e, it will not be checked by jre we will have to handle ourselves or tell jre to handle
     public static void main(String[] args) {
         System.out.println("Hello exception");  // Printed
         String str = null;
