64c826159bca2b193f6a03b0a38b040ae2d96eba
Divyam <divyamkumarsingh@gmail.com>
2020-04-28 12:57:19 +0530

    Divide by zero checked exceptio

---
+++ src/ExceptionEx1.java
@@ -0,0 +1,11 @@
+public class ExceptionEx1 {
+    // This is divide by zero exception
+    public static void main(String[] args) {
+        System.out.println("Hello Exceptions"); // This will be printed
+        System.out.println(10/0);
+        System.out.println("I seem to have exceptions"); //Since exceptions are caught prior to this this will not be printed
+        // These are checked exceptions where we will have to tell jre to provide default handler or
+        // we will have to handle the by ourselves
+
+    }
+}
